11ba27be0903419d84437156bdbb69c0f1e9e2f6
Tosif Bhatti <>
2023-03-11 15:39:43 +0530

    Solved Comment which are given In PR

--- CompanyProject/ViewController.swift
+++ CompanyProject/ViewController.swift
@@ -13,7 +13,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var email:UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var resultVal: UILabel!
+    
+    var id:String = "tosif@gmail.com"
+    var code:String = "tosif"
+    
+    @IBAction func btn(_ sender: UIButton) {
+        if (email.text == id) {
+            if pass.text == code {
+                resultVal.text="Valid User"
+            }
+        }
+    }
 }
 
